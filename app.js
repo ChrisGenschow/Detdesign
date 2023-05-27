c257893479b81ee7d1bc915ca30b8a113e798fa6
ChrisGenschow <91728844+ChrisGenschow@users.noreply.github.com>
2023-05-27 02:43:51 +0200

    Added pictures for scroll effect, changed animation slightly for easier readability

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ tl.to(".animated_eclipse", {
   opacity: 1,
   top: -200,
   scale: 5,
-  stagger: 2,
+  stagger: 2.4,
   delay: 1,
-}).to(".animated_eclipse", { display: "none" });
+})
+  .to(".animated_introtext", { display: "none" })
+  .to(".animated_eclipse", { display: "none", opacity: 0, duration: 0.5 });
 gsap.to(".animated_eclipse", { display: "block", opacity: 1 });
 gsap.to(".animated_introtext", {
   duration: 3,
   display: "block",
   delay: 0.5,
-  y: 400,
-  stagger: 2,
+  y: 340,
+  stagger: 2.4,
 });
