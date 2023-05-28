3975773450ad85346636bd38cd712f8e91f69f42
ChrisGenschow <91728844+ChrisGenschow@users.noreply.github.com>
2023-05-28 22:43:29 +0200

    Added text, a white ball and fonts
    2nd font does not work, and displays very low res, whilst the first one works.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function hideElement() {
   element.style.display = "none";
 }
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin, DrawSVGPlugin);
 
 tl.to(".animated_eclipse", {
   duration: 3,
@@ -15,9 +15,15 @@ tl.to(".animated_eclipse", {
   stagger: 2.4,
   delay: 1,
 })
-  .to(".animated_introtext", { display: "none" })
-  .to(".animated_eclipse", { display: "none", opacity: 0, duration: 0.5 });
+  .to(".animated_introtext", { display: "none", opacity: 0 })
+  .to(".white", { display: "none" })
+  .to(".herotext", { opacity: 1 })
+  .to("header", { display: "block", opacity: 1 })
+  .to(".animated_eclipse", { display: "none", opacity: 0, duration: 0.5 })
+  .to(".bounceball_background", { display: "block" });
+
 gsap.to(".animated_eclipse", { display: "block", opacity: 1 });
+
 gsap.to(".animated_introtext", {
   duration: 3,
   display: "block",
